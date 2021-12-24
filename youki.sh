3f8181e605ae0f4e7af1ee54647aa66e6020da9f
gattytto <emanaricua@gmail.com>
2021-12-24 16:35:58 +0100

    Update youki.sh

--- youki.sh
+++ youki.sh
@@ -10,27 +10,33 @@ apt install -y \
       libseccomp-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+cd /root/kubeinlxd
 wget -qO- https://github.com/containers/youki/zipball/main | bsdtar -xvf- -C ./ && mv containers-youki* youki
 cd youki && bash ./build.sh && cd ..
 cp youki/youki /usr/bin/ && chmod +x /usr/bin/youki
 rm -rf youki
 
-cat <<'EOF' >> /etc/crio/crio.conf
+if ! grep -q youki /etc/crio/crio.conf; then
+  cat <<'EOF' >> /etc/crio/crio.conf
 [crio.runtime.runtimes.youki]
 runtime_path = "/usr/bin/youki"
 runtime_type = "oci"
 runtime_root = "/run/youki"
 EOF
+fi
 
 systemctl reload crio
-export KUBECONFIG=/etc/kubernetes/admin.conf
-cat <<EOF | kubectl apply -f -
-apiVersion: node.k8s.io/v1
-kind: RuntimeClass
-metadata:
-  name: youki
-handler: youki
+if test -f "$FILE"; then
+  export KUBECONFIG=/etc/kubernetes/admin.conf
+  cat <<EOF | kubectl apply -f -
+  apiVersion: node.k8s.io/v1
+  kind: RuntimeClass
+  metadata:
+    name: youki
+  handler: youki
 EOF
+
+fi
 apt remove -y \
       curl               \
       libarchive-tools   \
