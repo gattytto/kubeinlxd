8ada73f708e85dc2aa8b38323b5c6f97dce4dd22
gat <asd@asd.com>
2023-07-01 20:22:07 +0000

    calico

--- metricbeat_configgen.sh
+++ metricbeat_configgen.sh
@@ -3,7 +3,7 @@ shopt -s expand_aliases
 
 set -euo pipefail
 NAMESPACE="elastic-system"
-MB_VERSION="7.16"
+MB_VERSION="v8.6.0"
 MB_MF="${1-metricbeat-kubernetes.yaml}"
 SEC_KEY_REF="elasticsearch-sample-es-elastic-user"
 echo "Printing to $MB_MF"
@@ -15,7 +15,7 @@ if [ ! $(which yq) ]; then
     echo "please install yq"
     exit 1
 fi
-yq eval "del(select(di == 2).spec.template.spec.containers[0].env[3].value)" -P -i metricbeat-kubernetes.yaml
-yq eval "select(di == 2).spec.template.spec.containers[0].env[3].valueFrom.secretKeyRef = {\"name\":\"$SEC_KEY_REF\",\"key\":\"elastic\"}" -P -i metricbeat-kubernetes.yaml
+yq eval "del(select(di == 9).spec.template.spec.containers[0].env[3].value)" -P -i metricbeat-kubernetes.yaml
+yq eval "select(di == 9).spec.template.spec.containers[0].env[3].valueFrom.secretKeyRef = {\"name\":\"$SEC_KEY_REF\",\"key\":\"elastic\"}" -P -i metricbeat-kubernetes.yaml
 yq eval "select(di != 4) .metadata.namespace = \"$NAMESPACE\"" -i "$MB_MF"
 
